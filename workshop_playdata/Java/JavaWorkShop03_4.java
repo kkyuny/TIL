0c45134e435e7d628711149930e6dedbc300b475
kyun <88278485+kkyuny@users.noreply.github.com>
2022-04-18 16:04:18 +0900

    Create JavaWorkShop03_4.java

---
+++ workshop_playdata/Java/JavaWorkShop03_4.java
@@ -0,0 +1,15 @@
+package workshop_java;
+
+public class JavaWorkShop03_4 {
+	public static void main(String[] args) {
+				
+		
+		for(int i=1; i<=6; i++) {
+			for(int j=1; j<=6; j++) {
+				if(i + j == 6) {
+					System.out.println(i + "+" + j + "=" +  (i+j));
+				}
+			}
+		}
+	}
+}
