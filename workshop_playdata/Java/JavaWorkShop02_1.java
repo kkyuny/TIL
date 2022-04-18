42f195b3303024b86a3821b2e944315affc6897d
kyun <88278485+kkyuny@users.noreply.github.com>
2022-04-18 10:59:22 +0900

    Create JavaWorkShop02_1.java

---
+++ workshop_playdata/Java/JavaWorkShop02_1.java
@@ -0,0 +1,24 @@
+package workshop_java;
+
+public class JavaWorkShop02_1 {
+	public static void main(String[] args) {
+		String s1 = "1";
+		String s2 = "2";
+		boolean bnx = false;
+		char c1 = 'A';
+		char c2 = 'B';
+		char c3 = '1';
+		char c4 = '2';
+		int inx = 2;
+		System.out.println(s1+s2);
+		System.out.println(!bnx);
+		System.out.println('A');
+		System.out.println('A' + 'B');
+		System.out.println('1');
+		System.out.println('1' + 2);
+		System.out.println('1' + '2');
+		
+		// 아스키 코드는 char을 이용하여 연산을 할 수 있다.
+		
+	}
+}
