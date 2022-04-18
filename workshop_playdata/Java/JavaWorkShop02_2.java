90ce8adcaa3ed92827908a14250537375f905254
kyun <88278485+kkyuny@users.noreply.github.com>
2022-04-18 11:10:25 +0900

    Create JavaWorkShop02_2.java

---
+++ workshop_playdata/Java/JavaWorkShop02_2.java
@@ -0,0 +1,15 @@
+package workshop_java;
+
+public class JavaWorkShop02_2 {
+	public static void main(String[] args) {
+		
+		int num = 456;
+		int result = num / 100 * 100;
+		float num2 = 456;
+		float result2 = num2 / 100 * 100;
+		System.out.println(result);
+		System.out.println(result2);
+		
+    // 정수를 나누면 결과값도 정수이다
+	}
+}
