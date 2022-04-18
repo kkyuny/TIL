0801de3801bd5275af924920662cd63a916ff3bf
kyun <88278485+kkyuny@users.noreply.github.com>
2022-04-18 11:16:16 +0900

    Create JavaWorkShop02_3.java

---
+++ workshop_playdata/Java/JavaWorkShop02_3.java
@@ -0,0 +1,11 @@
+package workshop_java;
+
+public class JavaWorkShop02_3 {
+	public static void main(String[] args) {
+		
+		char ch = 'z';
+		boolean b = ('a' <= ch && ch <= 'z');
+		System.out.println(b);
+		
+	}
+}
