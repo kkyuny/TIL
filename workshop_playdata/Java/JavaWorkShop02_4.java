8dca19671b7aa471de573ed9962d888e4a9b392a
kyun <88278485+kkyuny@users.noreply.github.com>
2022-04-18 11:17:41 +0900

    Create JavaWorkShop02_4.java

---
+++ workshop_playdata/Java/JavaWorkShop02_4.java
@@ -0,0 +1,12 @@
+package workshop_java;
+
+public class JavaWorkShop02_4 {
+	public static void main(String[] args) {
+		
+		int fahrenheit = 100;
+		float celcius = (float)5/9 * (fahrenheit-32);
+		System.out.println("Fahrenheit:"+fahrenheit);
+		System.out.println("Celcius:"+celcius);
+		
+	}
+}
