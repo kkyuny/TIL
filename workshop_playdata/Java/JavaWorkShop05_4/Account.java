15d0194a0454ffa5897a179b63076e7ade5c645e
kyun <88278485+kkyuny@users.noreply.github.com>
2022-04-20 16:10:50 +0900

    Add files via upload

---
+++ workshop_playdata/Java/JavaWorkShop05_4/Account.java
@@ -0,0 +1,33 @@
+package JavaWorkShop05_4;
+
+public class Account {
+	
+	private String account;
+	private int balance;
+	private double interestRate;
+	public Account() {
+		// TODO Auto-generated constructor stub
+	}
+	public Account(String account, int balance, double interestRate) {
+		this.account = account;
+		this.balance = balance;
+		this.interestRate = interestRate;
+	}
+	
+	public double calculateInterest() {
+		return balance * (100 + interestRate)/100;
+	}
+	public void deposit(int money) {
+		this.balance += money;
+	}
+	public void withdraw(int money) {
+		if (balance - money < 0) {
+			System.out.println("출금 할 수 없습니다.");
+		} else {
+			this.balance -= money;
+		}
+		
+	}
+	
+	
+}
