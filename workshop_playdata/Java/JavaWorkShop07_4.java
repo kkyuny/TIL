9c0579f85593a979ac29c31f50bca77be3fafa8b
kyun <88278485+kkyuny@users.noreply.github.com>
2022-04-21 15:23:32 +0900

    Create JavaWorkShop07_4.java

---
+++ workshop_playdata/Java/JavaWorkShop07_4.java
@@ -0,0 +1,26 @@
+package JavaWorkShop07_4;
+
+public class StringTest01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String strs = "I am second to none";
+		char[] arrs1 = strs.toCharArray();
+		String[] arrs2 = strs.split(" ");
+		
+		System.out.println(arrs2.length);
+		
+		int count = 0;
+		char[] chars = {};
+		for (char arrs : arrs1) {
+			if (arrs != ' ') {
+				count += 1;
+			}			
+		}
+		System.out.println(count);
+		
+
+	}
+
+
+}
