6ed886c910d1cee59bb1c4598ae823cc8c57f0bb
kyun <88278485+kkyuny@users.noreply.github.com>
2022-04-25 17:20:50 +0900

    Update workshop01.sql

--- workshop_playdata/DB/workshop01.sql
+++ workshop_playdata/DB/workshop01.sql
@@ -5,3 +5,16 @@ FROM TB_DEPARTMENT;
 -- 02
 SELECT '국문학과의 정원은 ' || capacity || '명 입니다.'
 FROM TB_DEPARTMENT;
+
+-- 03
+select department_no
+from tb_department
+where DEPARTMENT_NAME='국어국문학과';
+
+SELECT student_name
+FROM tb_student
+where absence_yn = 'Y' 
+and DEPARTMENT_NO = 001
+and substr(student_ssn,8,1)=2;
+
+
