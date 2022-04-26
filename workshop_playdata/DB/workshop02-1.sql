37ecfc276fd231f30399d8d1988231562376830d
kyun <88278485+kkyuny@users.noreply.github.com>
2022-04-26 15:21:39 +0900

    Update workshop02-1.sql

--- workshop_playdata/DB/workshop02-1.sql
+++ workshop_playdata/DB/workshop02-1.sql
@@ -8,3 +8,13 @@ order by 1;
 select professor_name, professor_ssn
 from tb_professor
 where length(professor_name) != 3;
+
+--3
+select professor_name 교수이름, 122-substr(professor_ssn,1,2) 나이
+from tb_professor
+where substr(PROFESSOR_SSN, 8, 1) = 1
+order by 2;
+
+--4
+select substr(professor_name, 2) as 이름
+from tb_professor;
