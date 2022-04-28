f657815d63b31045efa36e7885b176a4f70f47cc
kyun <88278485+kkyuny@users.noreply.github.com>
2022-04-28 16:23:43 +0900

    Update workshop03_01.sql

--- workshop_playdata/DB/workshop03_01.sql
+++ workshop_playdata/DB/workshop03_01.sql
@@ -10,3 +10,15 @@ from tb_student
 where (STUDENT_ADDRESS like '강원%'
 or STUDENT_ADDRESS like '경기도%')
 and STUDENT_NO like '9%';
+
+--18
+select professor_name, professor_ssn
+from tb_department tb, tb_professor tp
+where tb.department_no = tp.DEPARTMENT_NO
+and tb.DEPARTMENT_NAME = '법학과'
+order by tp.PROFESSOR_SSN;
+
+--19
+select class_name, department_name
+from tb_class tc, tb_department td
+where tc.department_no = td.DEPARTMENT_NO;
