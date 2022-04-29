1a7a947aa8805a8f98086f68150568ef7e982d89
kyun <88278485+kkyuny@users.noreply.github.com>
2022-04-29 19:53:18 +0900

    Update and rename workshop03_01.sql to workshop03-01.sql

--- workshop_playdata/DB/workshop03-01.sql
+++ workshop_playdata/DB/workshop03-01.sql
@@ -12,18 +12,40 @@ or STUDENT_ADDRESS like '경기도%')
 and STUDENT_NO like '9%';
 
 --18
+--oracle join
 select professor_name, professor_ssn
 from tb_department tb, tb_professor tp
 where tb.department_no = tp.DEPARTMENT_NO
 and tb.DEPARTMENT_NAME = '법학과'
 order by tp.PROFESSOR_SSN;
 
+--ansi join
+select professor_name, professor_ssn
+from tb_department tb NATURAL JOIN tb_professor tp
+where tb.DEPARTMENT_NAME = '법학과'
+order by tp.PROFESSOR_SSN;
+
 --19
+--oracle join
 select class_name, department_name
 from tb_class tc, tb_department td
 where tc.department_no = td.DEPARTMENT_NO;
 
+--ansi join
+select class_name, department_name
+from tb_class tc JOIN tb_department td
+on tc.department_no = td.DEPARTMENT_NO;
+
 --20
+--oracle join
 select class_name, professor_name
 from tb_class tc, tb_professor tp
 where tc.department_no = tp.DEPARTMENT_NO;
+
+--ansi join
+select class_name, professor_name
+from tb_class tc join tb_professor tp
+on tc.department_no = tp.DEPARTMENT_NO;
+
+--ansi join은 from절에 join할 테이블과 조건(using,on 등)을 함께 명시하여 사용할 수 있어 oracle join보다 가독성이 높다.
+--ansi join에서 join의 조건이 없는 경우 join의 형태(natural, left 등)를 명시해야한다
