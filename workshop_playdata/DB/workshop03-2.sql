86a342b234cbdad6a65ed312bdd45da3b3fa4fd1
kyun <88278485+kkyuny@users.noreply.github.com>
2022-04-30 23:50:02 +0900

    Update workshop03-2.sql

--- workshop_playdata/DB/workshop03-2.sql
+++ workshop_playdata/DB/workshop03-2.sql
@@ -12,3 +12,17 @@ where td.CATEGORY = '인문사회';
 -- 4개의 테이블을 join하였는데 하나의 테이블에서 다른 테이블과 join을 진행하고 on절에 fk로 조건을 추가하여
 -- 테이블이 이상없이 연결되도록 작성한다.
 -- using절을 이용할 수 있지만 개인적으로 on절이 가독성이 좋다고 판단되어 on절을 사용하였다.
+
+
+--22 
+select department_no
+from TB_DEPARTMENT
+where department_name = '음악학과';
+
+select ts.student_no 학번, ts.student_name "학생 이름", round(avg(point),1) "전체 평점"
+from TB_STUDENT ts join TB_GRADE tg
+on ts.STUDENT_NO = tg.STUDENT_NO
+where ts.DEPARTMENT_NO = '059'
+group by ts.student_no, ts.student_name;
+--전체 평점을 구하는 문제로 group by를 간단하게 이용하여 해결할 수 있었다.
+--하지만 년도별 평점 등의 다른 조건을 이용한 출력은 감이 안잡힌다.
