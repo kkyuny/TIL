026b33437e8ec31719f5fa7fe45a618e8121e5d0
kyun <88278485+kkyuny@users.noreply.github.com>
2022-05-10 23:46:16 +0900

    Update workshop05-1.sql

--- workshop_playdata/DB/workshop05-1.sql
+++ workshop_playdata/DB/workshop05-1.sql
@@ -7,10 +7,19 @@ CREATE TABLE TB_CATEGORY2
 
 DROP TABLE TB_CATEGORY2;
 
---31
+--30
 create table tb_class_type2
 (
   no varchar2(5) primary key,
   name varchar2(10)
 );
 DROP TABLE tb_class_type2;
+
+
+--32
+alter table tb_category
+modify (name not null);
+
+--33
+alter table tb_class_type
+modify (no varchar2(15), name varchar2(20));
